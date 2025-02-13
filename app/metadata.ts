fe44342eca957ec006e7a91136b8b92851a7e366
theartisancodes <kjoenzau@gmail.com>
2025-02-14 00:13:33 +0300

    update projects

--- app/metadata.ts
+++ app/metadata.ts
@@ -1,5 +1,4 @@
 export const metadata = {
   title: 'Crafted By Joe',
-  description: 'A Passionate Software Engineer & Problem-Solver\n' +
-    '\n'
+  description: 'A Passionate Software Engineer & Problem-Solver'
 };
